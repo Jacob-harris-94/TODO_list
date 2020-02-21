777de6798af0eff6ac613bc16f50fb8bf5c12b9c
Jacob Harris <jharris@gatech.edu>
2020-02-21 15:34:25 -0500

    update config to use pip freeze

---
+++ temp_init_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+postgres -D 'C:\Program Files\PostgreSQL\12\data' &
+createdb -U postgres todo
+
+# psql -U postgres todo # to connect
+
+#copy from windows as normal, fn+delete/insert to paste in bash.exe
+#https://dba.stackexchange.com/questions/83164/remove-password-requirement-for-user-postgres#83233
