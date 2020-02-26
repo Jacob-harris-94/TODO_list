01c017c8c50457c17d24aa2ccfe5e6eee4dc3779
Jacob Harris <jharris@gatech.edu>
2020-02-26 18:44:39 -0500

    convert db functions to context manager

--- src/dbinterface/interface.py
+++ src/dbinterface/interface.py
@@ -1,43 +1,37 @@
+from contextlib import contextmanager
 import psycopg2
+
+
+# https://book.pythontips.com/en/latest/context_managers.html
+@contextmanager
+def cur_conn(connect_data):
+    conn = psycopg2.connect("dbname=todo user=postgres")
+    cur = conn.cursor()
+    yield cur
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 # https://www.psycopg.org/docs/
 def init():
-    # Connect to an existing database
-    # Open a cursor to perform database operations
-    # Execute a command: this creates a new table
-    # cur.execute("CREATE TABLE todo.tag (id serial PRIMARY KEY, num integer, data varchar);")
-    # cur.execute("CREATE TABLE todo.tag_ref (id serial PRIMARY KEY, num integer, data varchar);")
-    # Make the changes to the database persistent
-    # Close communication with the database
-    conn = psycopg2.connect("dbname=todo user=postgres")
-    cur = conn.cursor()
-    cur.execute("DROP SCHEMA IF EXISTS todo CASCADE;")
-    cur.execute("CREATE SCHEMA IF NOT EXISTS todo;")
-    cur.execute("CREATE TABLE todo.item (id serial PRIMARY KEY, description text, time_created timestamp, time_due timestamp);")
-    conn.commit()
-    cur.close()
-    conn.close()
+    with cur_conn("dbname=todo user=postgres") as cur:
+        cur.execute("DROP SCHEMA IF EXISTS todo CASCADE;")
+        cur.execute("CREATE SCHEMA IF NOT EXISTS todo;")
+        cur.execute("CREATE TABLE todo.item (id serial PRIMARY KEY, description text, time_created timestamp, time_due timestamp);")
+
 
 def add_todo(todo_text="default text", due_date=None):
-    conn = psycopg2.connect("dbname=todo user=postgres")
-    cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO todo.item (description, time_created, time_due) VALUES (%s, CURRENT_TIMESTAMP, %s);",
-        (todo_text, due_date)
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
+    with cur_conn("dbname=todo user=postgres") as cur:
+        cur.execute(
+            "INSERT INTO todo.item (description, time_created, time_due) VALUES (%s, CURRENT_TIMESTAMP, %s);",
+            (todo_text, due_date)
+        )
 
 
 def get_list(num_items_to_get=3, sort_by_date_created=None, sort_by_date_due=None):
     print(f"trying to get {num_items_to_get} items...")
-    conn = psycopg2.connect("dbname=todo user=postgres")
-    cur = conn.cursor()
-    cur.execute("SELECT id, time_created::date, time_due::date, description FROM todo.item LIMIT %s;", (num_items_to_get,))
-    results =  cur.fetchall()
-    conn.commit()
-    cur.close()
-    conn.close()
+    with cur_conn("dbname=todo user=postgres") as cur:
+        cur.execute("SELECT id, time_created::date, time_due::date, description FROM todo.item LIMIT %s;", (num_items_to_get,))
+        results =  cur.fetchall()
     return results
