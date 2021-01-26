139324cc41350a08b59043bd9a7c0ec59dba9f76
jake (desktop) <jacob.harris.122@gmail.com>
2021-01-26 17:33:37 -0500

    misc changes

--- temp_init_script.sh
+++ temp_init_script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-postgres -D 'C:\Program Files\PostgreSQL\12\data' &
-createdb -U postgres todo
 
-# psql -U postgres todo # to connect
+#postgres -D 'C:\Program Files\PostgreSQL\12\data' &
+createdb -U postgres todo
+#dropdb -U postgres todo
+
+#psql -U  postgres todo # to connect
 
 #copy from windows as normal, fn+delete/insert to paste in bash.exe
 #https://dba.stackexchange.com/questions/83164/remove-password-requirement-for-user-postgres#83233
